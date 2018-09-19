0c6b97a69b4c78ecc3bf2594980991be90744638
Tony Feng <tony@v1020-wn-212-77.campus-dynamic.uwaterloo.ca>
2018-09-19 16:50:15 -0400

    dont mess up ls

--- views/index.jsx
+++ views/index.jsx
@@ -2,7 +2,7 @@ const React = require('react')
 
 class IndexView extends React.Component {
   render () {
-    return (<h1>Hello World! (in React)</h1>)
+    return (<h1>this will probably be a blog</h1>)
   }
 }
 
